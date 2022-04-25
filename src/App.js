ad1e9a55058cd60e2b2ff32ad8753131f5be3668
Axely7 <axely7@hotmail.com>
2022-04-25 09:08:11 -0500

    Creating styles

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {TodoCounter} from './TodoCounter';
+import { CreateTodoButton } from './CreateTodoButton';
+import { TodoItem } from './TodoItem';
+import { TodoSearch } from './TodoSearch';
+import { TodoList } from './TodoList';
+
+// import './App.css';
+const todos = [
+  {text: 'Cortar Cebolla', completed: false},
+  {text: 'Hacer de comer', completed: false},
+  {text: 'Hacer tarea', completed: false},
+]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+        <TodoCounter />
+          {/* <h2>Haz completado 2 de 3 TODOs</h2> */}
+        <TodoSearch />
+        <TodoList> 
+          {todos.map( todo => (
+            <TodoItem key={todo.text} text={todo.text}/>
+          ))}
+        </TodoList>
+        <CreateTodoButton /> 
+    </>
   );
 }
 
---
+++ src/TodoCounter.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './TodoCounter.css'
+
+// const estilos = {
+//     color: 'red',
+//     backgroundColor: 'yellow', 
+// }; 
+
+function TodoCounter(){
+    return(
+        // <h2 style={estilos}>Has completado 2 de 3 TODOs</h2>
+        <h2 className='TodoCounter'>Has completado 2 de 3 TODOs</h2>
+    );
+}
+
+export {TodoCounter}; 
